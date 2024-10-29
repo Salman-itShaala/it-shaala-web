a1657fc63136f5e69fc853c77c67f587f0af8759
salman-itShaala <salman.shaikh@itshaala.com>
2024-10-29 18:29:52 +0530

    completed testimonials section

--- app/components/sections/testimonials.tsx
+++ app/components/sections/testimonials.tsx
@@ -4,41 +4,52 @@ import TestimonialCard from '../testmimonial-card'
 
 const Testimonials = () => {
     return (
-        <div className='w-full bg-white dark:bg-black sm:px-20 px-2 py-16 flex gap-4 justify-center'>
-            <div className="flex flex-col gap-4 w-96">
-                {testimonils1.map((t, i) => {
-                    return <TestimonialCard
-                        key={t.message}
-                        studentName={t.studentName}
-                        studentBatch={t.studentBatch}
-                        message={t.message}
-                    />
-                })}
+        <>
+            <div className="w-full bg-white dark:bg-black sm:px-20 px-2 justify-center ">
+                <p className='text-blue-600 font-black pb-2'>
+                    Our happy students.
+                </p>
+                <p className='text-2xl font-black dark:text-slate-50 text-slate-950'>
+                    Read What our students are saying.
+                </p>
             </div>
-            <div className="flex flex-col gap-4 w-96">
-                {testimonils2.map((t, i) => {
-                    return <TestimonialCard
-                        key={t.message}
-                        studentName={t.studentName}
-                        studentBatch={t.studentBatch}
-                        message={t.message}
-                    />
-                })}
+            <div className='w-full bg-white dark:bg-black sm:px-20 px-2 py-16 flex gap-4 justify-center'>
+
+                <div className="flex flex-col gap-4 w-96">
+                    {testimonils1.map((t, i) => {
+                        return <TestimonialCard
+                            key={t.message}
+                            studentName={t.studentName}
+                            studentBatch={t.studentBatch}
+                            message={t.message}
+                        />
+                    })}
+                </div>
+                <div className="flex flex-col gap-4 w-96">
+                    {testimonils2.map((t, i) => {
+                        return <TestimonialCard
+                            key={t.message}
+                            studentName={t.studentName}
+                            studentBatch={t.studentBatch}
+                            message={t.message}
+                        />
+                    })}
+                </div>
+                <div className="flex flex-col gap-4 w-96">
+                    {testimonils3.map((t, i) => {
+                        return <TestimonialCard
+                            key={t.message}
+                            studentName={t.studentName}
+                            studentBatch={t.studentBatch}
+                            message={t.message}
+                        />
+                    })}
+                </div>
             </div>
-            <div className="flex flex-col gap-4 w-96">
-                {testimonils3.map((t, i) => {
-                    return <TestimonialCard
-                        key={t.message}
-                        studentName={t.studentName}
-                        studentBatch={t.studentBatch}
-                        message={t.message}
-                    />
-                })}
-            </div>
-        </div>
+        </>
+
     )
 }
-
 
 export default Testimonials
 
